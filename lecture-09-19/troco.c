de0cff7e2e03d6ee643d95d69febd1c147bb2af5
jmartins3 <jmartins@isel.pt>
2024-09-23 18:55:34 +0100

    troco enhanced

--- lecture-09-19/troco.c
+++ lecture-09-19/troco.c
@@ -8,49 +8,79 @@ int main() {
 	
 	int nCoins;
 	
+	// 2 euros
 	nCoins = exchange / 200; // calcular número de moedas de 2 euros
-	printf("%d moedas de 2 euros\n", nCoins);
+	
+	if (nCoins > 0) {
+		printf("%d moedas de 2 euros\n", nCoins);
+	}
 	
 	//exchange = exchange - 200 * nCoins; // cálculo do resto
 	exchange = exchange % 200; // resto da divisão em C
+	
+	// 1 euro
 	
 	nCoins = exchange / 100; // calcular número de moedas de 1 euros
 	
 	if (nCoins > 0) {
 		printf("%d moedas de 1 euro\n", nCoins);
 	}
-	
-	
-	//exchange = exchange - 100 * nCoins; // cálculo do resto
+	 
 	exchange = exchange % 100; // resto da divisão em C
 	
+	// 50 cêntimos
 	nCoins = exchange / 50; // calcular número de moedas de 50 cêntimos
 	
-	printf("%d moedas de 50 cêntimos\n", nCoins);
+	if (nCoins > 0) {
+		printf("%d moedas de 50 cêntimos\n", nCoins);
+	}
 	
-	exchange = exchange - 50 * nCoins; // cálculo do resto
+	exchange = exchange % 50;   // cálculo do resto
 	
+	
+	// 20 cêntimos
 	nCoins = exchange / 20; // calcular número de moedas de 20 cêntimos
-	printf("%d moedas de 20 cêntimos\n", nCoins);
 	
-	exchange = exchange - 20 * nCoins; // cálculo do resto
+	if (nCoins > 0) {
+		printf("%d moedas de 20 cêntimos\n", nCoins);
+	}
+	
+	exchange = exchange % 20; // cálculo do resto
+	
+	// 10 cêntimos  
 	
 	nCoins = exchange / 10; // calcular número de moedas de 10 cêntimos
-	printf("%d moedas de 10 cêntimos\n", nCoins);
 	
-	exchange = exchange - 10 * nCoins; // cálculo do resto
+	if (nCoins > 0) {
+		printf("%d moedas de 10 cêntimos\n", nCoins);
+	}
+	
+	exchange = exchange % 10; // cálculo do resto
+	
+	// 5 cêntimos
 	
 	nCoins = exchange / 5; // calcular número de moedas de 5 cêntimos
-	printf("%d moedas de 5 cêntimos\n", nCoins);
 	
-	exchange = exchange - 5 * nCoins; // cálculo do resto
+	if (nCoins > 0) {
+		printf("%d moedas de 5 cêntimos\n", nCoins);
+	}
+	
+	exchange = exchange % 5; // cálculo do resto
+	
+	
+	// 2 cêntimos
 	
 	nCoins = exchange / 2; // calcular número de moedas de 2 cêntimos
-	printf("%d moedas de 2 cêntimos\n", nCoins);
 	
-	exchange = exchange - 2 * nCoins; // cálculo do resto
+	if (nCoins > 0) {
+		printf("%d moedas de 2 cêntimos\n", nCoins);
+	}
 	
-	printf("%d moedas de 1 cêntimos\n", exchange);
+	exchange = exchange % 2; // cálculo do resto
+	
+	if (exchange > 0) {
+		printf("%d moedas de 1 cêntimos\n", exchange);
+	}
 	
 	return 0;
 }
